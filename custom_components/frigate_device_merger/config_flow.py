788a9ec2d2c20a282d2801634645e17eea9b24b6
Josiah Clark <josiahclark@MacBookPro.localdomain>
2025-12-11 20:17:25 +1300

    Reorganize files for HACS compliance

---
+++ custom_components/frigate_device_merger/config_flow.py
@@ -0,0 +1,20 @@
+"""Config flow for Frigate Device Merger."""
+from __future__ import annotations
+
+from homeassistant import config_entries
+
+from . import DOMAIN
+
+
+class FrigateDeviceMergerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Frigate Device Merger."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        # This integration doesn't need user input, just create the entry
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+        
+        return self.async_create_entry(title="Frigate Device Merger", data={})
